4b23885fb198b99b3c6e8cd8e183425329d91a11
yq <yiqunw0907@gmail.com>
2019-07-23 15:28:08 -0400

    feature: full home service compelete:
    1. Launch Gazebo
    2. Launch Rviz
    3. Adaptive Monte Carlo Localization package
    4. Virtual objects set in Rviz
    5. Navigation package kick in

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -5,5 +5,6 @@ xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch " &
 sleep 3
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 3
-xterm  -e  " rosrun add_markers add_markers "
-
+xterm  -e  " rosrun add_markers add_markers "&
+sleep 3
+xterm -e " rosrun pick_objects pick_objects "
