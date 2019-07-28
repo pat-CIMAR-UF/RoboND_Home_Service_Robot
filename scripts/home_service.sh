0e6981edaf42f9b0edc0c815f076f5b67b19ff39
Yiqun <yiqunw0907@gmail.com>
2019-07-27 22:04:43 -0400

    Changes to be committed: 	new file:   my_robot/rvizConfig/navigation.rviz 	modified:   scripts/home_service.sh

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -3,7 +3,7 @@ xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospa
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find my_robot)/world/mymap.yaml " &
 sleep 5
-xterm -e " rosrun rviz rviz -d /home/yq/catkin_ws/src/RoboND_Home_Service_Robot/rvizConfig/navigation.rviz " &
+xterm -e " rosrun rviz rviz -d $(rospack find my_robot)/rvizConfig/navigation.rviz " &
 sleep 3
 xterm -e "rosrun add_markers add_markers " &
 sleep 5
